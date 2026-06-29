aef46ff46f876ce4ed644cd4f7ca381c3b5adab7
raj.singh2005 <raj.singh2005.dev@gmail.com>
2026-06-29 16:15:02 +0530

    created User Schema and Video Schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,31 +1,52 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    index: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "username is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: [true, "fullName is required"],
+      lowercase: true,
+      trim: true,
+    },
+    avatar: {
+      type: String, // cloudinary url
+      required: [true, "avatar is required"],
+    },
+    coverImage: {
+      type: String, // cloudinary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  fullname: {
-    type: String,
-    required: true,
-    lowercase: true,
-    trim: true,
-  },
-  avatar: {
-    type: String,
-  },
-  
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const User = mongoose.model("User", userSchema);
