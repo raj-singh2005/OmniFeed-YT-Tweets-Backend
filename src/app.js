3a8d2a943660b164b7e88463715683693df2e019
raj.singh2005 <raj.singh2005.dev@gmail.com>
2026-06-29 12:23:35 +0530

    server setup and custom error and response handler

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
+const app = express();
+
+app.use(cors({
+    origin : process.env.CORS_ORIGIN || "*" ,
+    credentials : true
+})) ;
+
+app.use(express.json({
+    limit: "10kb"
+})) ;
+
+app.use(express.urlencoded({
+    extended: true ,
+    limit : "10kb"
+})) ;
+
+app.use(express.static(path.join(process.cwd(), "public"))) ;
+
+app.use(cookieParser()) ;
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-import dotenv from "dotenv";
-import connectDB from "./db/index.js";
-dotenv.config({ path: "./.env" });
+import "./config.js";
 
-connectDB();
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    const server = app.listen(PORT, () => {
+      console.log(`server is running on port : ${PORT}`);
+    });
+
+    server.on("error", (err) => {
+      console.log("Error", err);
+      process.exit(1);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection Failed", err);
+    process.exit(1);
+  });
