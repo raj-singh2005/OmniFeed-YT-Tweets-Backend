59921675e4457fd7fc4461acf92f53deca12c7c6
raj.singh2005 <raj.singh2005.dev@gmail.com>
2026-07-08 11:38:15 +0530

    added subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "subscriber is required"],
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "channel is required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
