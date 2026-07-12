34232458577c1d0a7e9f653514e43b978df5048d
raj.singh2005 <raj.singh2005.dev@gmail.com>
2026-07-13 02:34:05 +0530

    feat: add healthcheck route to app for improved server monitoring

--- src/app.js
+++ src/app.js
@@ -32,12 +32,14 @@ app.use(cookieParser());
 //routes
 import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
-import tweetRouter from "./routes/tweet.router.js"
+import tweetRouter from "./routes/tweet.router.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 
 // routes declaration
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
-app.use("/api/v1/tweet",  tweetRouter)
+app.use("/api/v1/tweet", tweetRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter)
 
 export { app };
