984ef3b379727dbc0c5c0e035a4cbbfa44969407
raj.singh2005 <raj.singh2005.dev@gmail.com>
2026-07-12 22:24:22 +0530

    style: format app.js for consistency and readability

--- src/app.js
+++ src/app.js
@@ -5,31 +5,37 @@ import path from "path";
 
 const app = express();
 
-app.use(cors({
-    origin : process.env.CORS_ORIGIN || "*" ,
-    credentials : true
-})) ;
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "*",
+    credentials: true,
+  })
+);
 
-app.use(express.json({
-    limit: "10kb"
-})) ;
+app.use(
+  express.json({
+    limit: "10kb",
+  })
+);
 
-app.use(express.urlencoded({
-    extended: true ,
-    limit : "10kb"
-})) ;
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "10kb",
+  })
+);
 
-app.use(express.static(path.join(process.cwd(), "public"))) ;
+app.use(express.static(path.join(process.cwd(), "public")));
 
-app.use(cookieParser()) ;
+app.use(cookieParser());
 
 //routes
 import userRouter from "./routes/user.routes.js";
-import videoRouter from "./routes/video.routes.js"
+import videoRouter from "./routes/video.routes.js";
 
-// routes declaration 
+// routes declaration
 
-app.use("/api/v1/users",userRouter) ; 
-app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
 
 export { app };
