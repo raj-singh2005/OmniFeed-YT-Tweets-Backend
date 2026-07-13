618a3f6dd164989388f0282d92be25ee1420e51d
raj.singh2005 <raj.singh2005.dev@gmail.com>
2026-07-13 20:48:05 +0530

    feat: add like routes for handling likes in the application

--- src/app.js
+++ src/app.js
@@ -35,6 +35,7 @@ import videoRouter from "./routes/video.routes.js";
 import tweetRouter from "./routes/tweet.router.js";
 import healthcheckRouter from "./routes/healthcheck.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import likeRouter from "./routes/like.routes.js";
 
 // routes declaration
 
@@ -43,5 +44,6 @@ app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/tweet", tweetRouter);
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/likes", likeRouter);
 
 export { app };
