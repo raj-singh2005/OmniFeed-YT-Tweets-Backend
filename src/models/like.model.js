29bb36cf4c117e1671d6043d4df51239a79a7402
raj.singh2005 <raj.singh2005.dev@gmail.com>
2026-07-13 16:40:10 +0530

    feat: add mongoose aggregate paginate plugin to Like and Tweet models

--- src/models/like.model.js
+++ src/models/like.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
 
 
 const likeSchema = new Schema({
@@ -20,5 +22,7 @@ const likeSchema = new Schema({
     },
     
 }, {timestamps: true})
+
+likeSchema.plugin(mongooseAggregatePaginate)
 
 export const Like = mongoose.model("Like", likeSchema)
